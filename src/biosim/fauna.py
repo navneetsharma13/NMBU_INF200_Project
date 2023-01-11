85b4d3eada6eaa6fe182711ac8ad92ffc55bce3c
sharma_navneet <navneet.sharma@nmbu.co>
2023-01-11 22:06:18 +0100

    Updated few file

--- src/biosim/fauna.py
+++ src/biosim/fauna.py
@@ -33,16 +33,16 @@ class Fauna:
             # self.raise_type_error(weight)
             self.weight = weight
 
-        self.fitness = None
-        self.calculate_fitness(self.age,self.weight,self.parameters)
+        self.fitness = self.calculate_fitness(self.age, self.weight, self.parameters)
 
-    def raise_type_error(val):
-        if isinstance(val, (int, float)) is False:
-            raise ValueError(str(val) + "cannot be a value type,it has to be int or float")
+    # def raise_type_error(val):
+    #     if isinstance(val, (int, float)) is False:
+    #         raise ValueError(str(val) + "cannot be a value type,it has to be int or float")
 
     def weight(self):
         return self.weight()
 
+    @classmethod
     def weight_default(cls):
         mu = math.log(cls.parameters['w_birth'] ** 2 / math.sqrt(
             cls.parameters['w_birth'] ** 2 + cls.parameters['sigma_birth']))
@@ -53,22 +53,25 @@ class Fauna:
         self.age += 1
 
     def weight_decrease(self):
-        self._weight = self.weight - (self.weight * self.parameters['eta'])
-        self.fitness = self.calculate_fitness(self.age,self.weight,self.parameters)
+        self.weight = self.weight - (self.weight * self.parameters['eta'])
+        self.fitness = self.calculate_fitness(self.age, self.weight, self.parameters)
 
-
-
-    def calculate_fitness(cls,age,weight,parameters):
+    @classmethod
+    def calculate_fitness(cls, age, weight, parameters):
         def fitness_formula(sign, x, x_half, phi_x):
 
-            return 1.0/(1 + math.exp(sign * phi_x * (x - x_half)))
+            return 1.0 / (1 + math.exp(sign * phi_x * (x - x_half)))
 
         if weight == 0:
-            phi = 0
+            fitness = 0
         else:
-            phi = fitness_formula(1,age,parameters['a_half'],parameters['phi_age'])* fitness_formula(-1,weight,parameters['w_half'],parameters['phi_weight'])
-            # cls.check_phi_borders(phi)
-            return phi
+            fitness = fitness_formula(1, age, parameters['a_half'],
+                                      parameters['phi_age']) * fitness_formula(-1, weight,
+                                                                               parameters['w_half'],
+                                                                               parameters[
+                                                                                   'phi_weight'])
+            # cls.check_phi_borders(fitness)
+        return fitness
 
     @classmethod
     # def check_phi_borders(cls,phi):
@@ -79,22 +82,19 @@ class Fauna:
     #     migrate_prob = self.parameters['mu'] * self.fitness
     #     if migrate_prob > random.random():
     #         return True
-
-
     def birth_prob(self, animal_number):
         zeta = self.parameters['zeta']
         w_birth = self.parameters['w_birth']
         sigma_birth = self.parameters['sigma_birth']
         gamma = self.parameters['gamma']
         if self.weight >= zeta * (w_birth + sigma_birth):
-            return random.random() < min(1, gamma *
-                                         self.fitness * (animal_number))
+            return random.random() < min(1, gamma * self.fitness * (animal_number - 1))
         else:
             return False
 
     def weight_decrease_on_birth(self, child):
         if self.weight >= child.weight * child.parameters['xi']:
-            self._weight -= child.weight * child.parameters['xi']
+            self.weight -= child.weight * child.parameters['xi']
 
     def die(self):
         if self.weight is None:
@@ -111,25 +111,25 @@ class Fauna:
 
         self.weight += amount * self.parameters['beta']
 
+
 class Herbivore(Fauna):
+    eta = 0.05
+    F = 10.0
+    beta = 0.9
+    w_birth = 8.0
+    sigma_birth = 1.5
+    phi_age = 0.2
+    phi_weight = 0.1
+    a_half = 40
+    w_half = 10.0
+    gamma = 0.8
+    zeta = 3.5
+    xi = 1.2
+    mu = 0.25
+    lambda_ = 1.0
+    omega = 0.4
 
-    eta=0.05
-    F=10.0
-    beta=0.9
-    w_birth=8.0
-    sigma_birth=1.5
-    phi_age=0.2
-    phi_weight=0.1
-    a_half=40
-    w_half=10.0
-    gamma=0.8
-    zeta=3.5
-    xi=1.2
-    mu=0.25
-    lambda_=1.0
-    omega=0.4
-
-    parameters = {'eta':eta, 'F': F, 'beta': beta, 'w_birth': w_birth,
+    parameters = {'eta': eta, 'F': F, 'beta': beta, 'w_birth': w_birth,
                   'sigma_birth': sigma_birth, 'phi_age': phi_age, 'phi_weight': phi_weight,
                   'a_half': a_half, 'w_half': w_half, 'gamma': gamma, 'zeta': zeta,
                   'xi': xi, 'mu': mu, 'lambda': lambda_, 'omega': omega
@@ -138,24 +138,24 @@ class Herbivore(Fauna):
     def __init__(self, age=None, weight=None):
         super().__init__(weight, age)
 
-class Carnivore(Fauna):
 
-    eta=0.125
-    F=50.0
-    beta=0.75
-    w_birth=6.0
-    sigma_birth=1.0
-    phi_age=0.4
-    phi_weight=0.4
-    a_half=60
-    w_half=4.0
-    gamma=0.8
-    zeta=3.5
-    xi=1.1
-    mu=0.4
-    DeltaPhiMax=10.0
-    lambda_=1.0
-    omega=0.9
+class Carnivore(Fauna):
+    eta = 0.125
+    F = 50.0
+    beta = 0.75
+    w_birth = 6.0
+    sigma_birth = 1.0
+    phi_age = 0.4
+    phi_weight = 0.4
+    a_half = 60
+    w_half = 4.0
+    gamma = 0.8
+    zeta = 3.5
+    xi = 1.1
+    mu = 0.4
+    DeltaPhiMax = 10.0
+    lambda_ = 1.0
+    omega = 0.9
 
     parameters = {'eta': eta, 'F': F, 'beta': beta, 'w_birth': w_birth,
                   'sigma_birth': sigma_birth, 'phi_age': phi_age, 'phi_weight': phi_weight,
