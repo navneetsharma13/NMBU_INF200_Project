0103e4aca2c1c907f37497f4eda0dfdc1e40aa2d
sushant123 <sushant-kumar-srivastava@nmbu.no>
2023-01-11 12:24:55 +0100

    Pushing the animal scripts

--- src/biosim/map.py
+++ src/biosim/map.py
@@ -3,3 +3,16 @@ from .animal import herbivore,carnivore
 
 class Map:
     def __init__(self,island_map):
+        self._map=island_map
+        self._landscape_classes={
+            'H':highland,
+            'L':lowland,
+            'D':desert,
+            'W':water
+        }
+        self._animal_classes={'Carnivore':carnivore,
+                              'Herbivore':herbivore
+
+        }
+
+    def _create_cell(self):
