8bfa705f28f3c91bb081b9440773fcb11ca9ddaf
sushant123 <sushant-kumar-srivastava@nmbu.no>
2023-01-22 11:23:41 +0100

    adding test_map.py with additional functions

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,5 +1,7 @@
 __author__ = "Navneet Sharma and Sushant Kumar Srivastava"
-__email__ = "navneet.sharma@nmbu.no and sushant.kumar.sirvastava@nmbu.no"
+__email__ = "navneet.sharma@nmbu.no and sushant.kumar.srivastava@nmbu.no"
+
+import random
 
 import pytest
 from biosim.simulation import BioSim
@@ -8,6 +10,8 @@ import textwrap
 import pytest
 from src.biosim.simulation import BioSim
 from src.biosim.map import Map
+from src.biosim.fauna import Herbivore,Carnivore
+from src.biosim.landscape import Landscape
 
 
 def geo_list(island_map):
@@ -24,29 +28,30 @@ def geo_list(island_map):
            WWWWW""", """\
            WWWWW
            DLWWW
-           WWWWW""","""\
+           WWWWW""", """\
            LWWWWWWWWWWWW
            WLWWWLLLLLLLL
            WWWWWDDDDDDDD"""])
 def test_check_invalid_boundaries(map_str):
-
     with pytest.raises(ValueError):
         m = Map(map_str)
         # assert err.type is ValueError
+
+
 @pytest.mark.parametrize('map_str', ["""\
            WWWW
            WLWWW
            WWWWW""", """\
            WWWWW
            WW
-           WWWWW""","""\
+           WWWWW""", """\
            WWWWWWWWWWWWWWWWWWWWW
            WLWWW
            WWWWW"""])
 def test_check_invalid_line_length(map_str):
-
     with pytest.raises(ValueError):
         m = Map(map_str)
+
 
 @pytest.mark.parametrize('map_str', ["""\
            WWWW@
@@ -54,37 +59,18 @@ def test_check_invalid_line_length(map_str):
            WWWWW""", """\
            WWWWW
            D.WWW
-           WWWWW""","""\
+           WWWWW""", """\
            LWWWWWWWWWWWW
            WLWWWLL?LLLLL
            WWWWWDDDDDDDD"""])
 def test_check_invalid_character(map_str):
-
     with pytest.raises(ValueError):
         m = Map(map_str)
 
-@pytest.mark.parametrize("age, weight", [(10, 20)])
-def create_map_for_test(self, age, weight):
-    geogr = """\
-               WWW
-               WLW
-               WWW"""
-    geogr = textwrap.dedent(geogr)
-    ini_herbs = [
-        {
-            "loc": (2, 2),
-            "pop": [{"species": "Herbivore", "age": age, "weight": weight}],
-        }]
 
-    seed = 123213
-    t_sim = BioSim(geogr, ini_herbs, seed)
-    loc = (1, 1)
-
-    return t_sim, loc
-
-
+@pytest.fixture(autouse=True)
 def test_cell_list():
-    island_maps="""\
+    island_maps = """\
                WWW
                WLW
                WWW"""
@@ -92,11 +78,115 @@ def test_cell_list():
     list_of_cells = geo_list(island_maps)
     assert list_of_cells[1][1] is 'L'
 
-def test_age_weight_stored(self):
-    t, loc = self.create_map_for_test()
-    h_weight = t.Map.livable_cells_calculate[loc].initial_population['Herbivore'][0].age
-    assert h_weight is 20
-    h_age = t.Map.livable_cells_calculate[loc].initial_population['Herbivore'][0].weight
-    assert h_age is 15
+@pytest.fixture(autouse=True)
+def create_map_for_test(age=10,weight=20):
+        geogr = """\
+                   WWW
+                   WLW
+                   WWW"""
+        geogr = textwrap.dedent(geogr)
+        ini_herbs = [
+            {
+                "loc": (2, 2),
+                "pop": [{"species": "Herbivore", "age": age, "weight": weight}],
 
-# def test_get_population_numbers(self):
+            }]
+
+        ini_carns = [
+            {
+                "loc": (2, 2),
+                "pop": [{"species": "Carnivore", "age": age, "weight": weight}],
+
+            }]
+
+        seed = 123213
+        t_sim = BioSim(geogr, ini_herbs, seed)
+        t_sim.add_population(ini_carns)
+        loc = (1, 1)
+
+        return t_sim, loc
+
+
+
+def test_age_weight_stored(create_map_for_test):
+
+    t_sim,loc=create_map_for_test
+    h1_age=t_sim.map.livable_cell_calculate()[loc].initial_population['Herbivore'][0].age
+    h1_age_compare=t_sim.map.get_pop_age_herb()[0]
+    assert h1_age is h1_age_compare
+
+    h1_weight = t_sim.map.livable_cell_calculate()[loc].initial_population['Herbivore'][0].weight
+    h1_weight_compare=t_sim.map.get_pop_weight_herb()[0]
+    assert h1_weight is h1_weight_compare
+
+    c1_age=t_sim.map.livable_cell_calculate()[loc].initial_population['Carnivore'][0].age
+    c1_age_compare=t_sim.map.get_pop_age_carn()[0]
+    assert c1_age is c1_age_compare
+
+    c1_weight = t_sim.map.livable_cell_calculate()[loc].initial_population['Carnivore'][0].weight
+    c1_weight_compare=t_sim.map.get_pop_weight_carn()[0]
+    assert c1_weight is c1_weight_compare
+
+def test_get_population_numbers(create_map_for_test):
+    t_sim,loc=create_map_for_test
+
+    assert t_sim.map.get_pop_tot_num_herb()+t_sim.map.get_pop_tot_num_carn() == t_sim.map.get_pop_tot_num()
+
+def test_pop_matrix_herbivore(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    assert t_sim.map.get_pop_matrix_herb()[loc[0]][loc[1]] is t_sim.map.get_pop_tot_num_herb()
+def test_pop_matrix_carnivore(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    assert t_sim.map.get_pop_matrix_carn()[loc[0]][loc[1]] is t_sim.map.get_pop_tot_num_carn()
+
+def test_unique_row(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    assert len(set(t_sim.map.calculate_animal_count()['Row_no'])) is len(t_sim.map.unique_rows())
+def test_unique_column(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    assert len(set(t_sim.map.calculate_animal_count()['Col_no'])) is len(t_sim.map.unique_columns())
+
+def test_add_population(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    pop_before_herb= t_sim.map.get_pop_tot_num_herb()
+    ini_herbs_1 = [
+            {
+                "loc": (2, 2),
+                "pop": [{"species": "Herbivore", "age": 24, "weight": 60}],
+
+            }]
+    t_sim.add_population(ini_herbs_1)
+    pop_after_herb=t_sim.map.get_pop_tot_num_herb()
+    assert pop_after_herb is pop_before_herb+1
+
+    pop_before_carn= t_sim.map.get_pop_tot_num_carn()
+    ini_carns_1 = [
+            {
+                "loc": (2, 2),
+                "pop": [{"species": "Carnivore", "age": 31, "weight": 50}],
+
+            }]
+    t_sim.add_population(ini_carns_1)
+    pop_after_carn=t_sim.map.get_pop_tot_num_carn()
+    assert pop_after_carn is pop_before_carn+1
+
+def test_create_neighbours(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    len_neighbours_at_loc_01=len(t_sim.map.create_neighbours_dict()[loc[0]-1,loc[0]])
+    assert len_neighbours_at_loc_01 is 1
+
+def test_migrate_cell_calculate(create_map_for_test):
+    t_sim,loc=create_map_for_test
+    location_migration_cells=t_sim.map.migrate_cell_calculate().keys()
+    assert list(location_migration_cells) == [loc]
+
+
+
+
+
+
+
+
+
+
+
